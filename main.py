bb44ec2976bafbe1e79cfb86d2bceec9e819601d
NothAmor <nothamor@NothAmordeMBP.lan>
2021-07-12 15:25:51 +0800

    Initial commit

--- main.py
+++ main.py
@@ -24,33 +24,36 @@ def loads_str(data_str):
             # 该处将处理结果继续递归处理
             return loads_str(data_str)
 
-for i in range(1, 3934):
+for i in range(1, 30000):
     # 请求链接
     response = requests.get(url.format(i))
     # 加载数据，过滤escape
     question_data = loads_str(response.text)
     print("正在爬取链接：{}".format(url.format(i)))
 
-    # 如果题目中有图片，进行图片的爬取
-    if question_data["imageurl"]:
-        print("监测到题目中含有图片，正在爬取...")
-        pic_response = requests.get(question_data["imageurl"])
-        pic_name = question_data["imageurl"].split("/")[-1][0:-4]
-        with open("/www/wwwroot/qr.nothamor.cn/questions/{}.jpg".format(pic_name), "wb+") as pic:
-            pic.write(pic_response.content)
-        question_data["imageurl"] = "https://xxx.com/{}.jpg".format(pic_name)
+    if question_data["question"] != "":
+        # 如果题目中有图片，进行图片的爬取
+        if question_data["imageurl"]:
+            print("监测到题目中含有图片，正在爬取...")
+            pic_response = requests.get(question_data["imageurl"])
+            pic_name = question_data["imageurl"].split("/")[-1][0:-4]
+            with open("/www/wwwroot/questions/{}.jpg".format(pic_name), "wb+") as pic:
+                pic.write(pic_response.content)
+            question_data["imageurl"] = "https://xxx.com/{}.jpg".format(pic_name)
 
-    print("正在爬取第{}道题，id为{}, 题目为{}, 答案为：{}".format(i, question_data["id"], question_data["question"], question_data["ta"]))
-    sql = """insert into questions(question, a, b, c, d, ta, imageurl, bestanswer, bestanswerid, type, sinaimg, options)
-    values("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")
-    ;""".format(question_data["question"], question_data["a"], question_data["b"], question_data["c"], question_data["d"], question_data["ta"], question_data["imageurl"], question_data["bestanswer"], question_data["bestanswerid"], question_data["Type"], question_data["sinaimg"], question_data["options"])
-    # with open("./question{}.json".format(i), "wb+") as file:
-    #     file.write(response.text.encode())
-    try:
-        # 执行sql语句
-        cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
+        print("正在爬取第{}道题，id为{}, 题目为{}, 答案为：{}".format(i, question_data["id"], question_data["question"], question_data["ta"]))
+        sql = """insert into questions(question, a, b, c, d, ta, imageurl, bestanswer, bestanswerid, type, sinaimg, options)
+        values("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")
+        ;""".format(question_data["question"], question_data["a"], question_data["b"], question_data["c"], question_data["d"], question_data["ta"], question_data["imageurl"], question_data["bestanswer"], question_data["bestanswerid"], question_data["Type"], question_data["sinaimg"], question_data["options"])
+        # with open("./question{}.json".format(i), "wb+") as file:
+        #     file.write(response.text.encode())
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            db.commit()
+        except:
+            # 如果发生错误则回滚
+            db.rollback()
+    else:
+        continue
